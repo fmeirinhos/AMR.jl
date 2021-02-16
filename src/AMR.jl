491f06714272f64b49893e52bce50729ba7d8f8b
Francisco Meirinhos <fcmeirinhos@gmail.com>
2021-02-16 10:40:31 +0000

    Cleanup

--- src/AMR.jl
+++ src/AMR.jl
@@ -1,12 +1,8 @@
 module AMR
 
-include("sampling.jl")
 export sample_function
 
 include("utils.jl")
+include("sampling.jl")
 
-# using Parameters
-# using StaticArrays
-# include("AbstractTrees.jl/src/AbstractTrees.jl")
-# include("tree.jl")
 end # module
